8723b940025ac03bbfc7f8fdf9471e2ba334a1c6
MattTheCuber <mattvine03@gmail.com>
2024-05-18 19:48:23 -0400

    feat: start exercise 1

---
+++ 02 Graphical User Interface Assignment - Exercise 1/src/MainFrame.java
@@ -0,0 +1,140 @@
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainFrame extends JFrame {
+    String[] spamKeyPhrases = {
+            "As seen on",
+            "Buy",
+            "Meet singles",
+            "Extra income",
+            "Million dollars",
+            "Save big money",
+            "Opportunity",
+            "Cash",
+            "No fees",
+            "Online degree",
+            "Work at home",
+            "Additional income",
+            "Eliminate debt",
+            "Lower interest rate",
+            "Pre-approved",
+            "Consolidate your debt",
+            "Avoid bankruptcy",
+            "Miracle",
+            "Satisfaction",
+            "Risk free",
+            "Free hosting",
+            "You have been selected",
+            "Weekend getaway",
+            "You're a Winner!",
+            "Offer",
+            "Unlimited",
+            "No obligation",
+            "Trial",
+            "Guarantee",
+            "No purchase necessary"
+    };
+    JLabel titleLabel;
+    JLabel textAreaLabel;
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    JButton clearButton;
+    JButton verifyButton;
+    JPanel textPanel;
+    JPanel buttonPanel;
+    JPanel mainPanel;
+
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.initComponents();
+    }
+
+    public void initComponents() {
+        titleLabel = new JLabel("Spam Score Calculator");
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+        textAreaLabel = new JLabel("Type or paste email text below:");
+        textArea = new JTextArea();
+        scrollPane = new JScrollPane(textArea);
+        clearButton = new JButton("Clear");
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearButtonClicked(e);
+            }
+        });
+        verifyButton = new JButton("Verify");
+        verifyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                verifyButtonClicked(e);
+            }
+        });
+
+        textPanel = new JPanel();
+        textPanel.setLayout(new BorderLayout(10, 10));
+        textPanel.add(textAreaLabel, BorderLayout.NORTH);
+        textPanel.add(scrollPane, BorderLayout.CENTER);
+
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.add(clearButton);
+        buttonPanel.add(verifyButton);
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout(10, 10));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        mainPanel.add(titleLabel, BorderLayout.NORTH);
+        mainPanel.add(textPanel, BorderLayout.CENTER);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+        add(mainPanel);
+
+        setTitle("02 Graphical User Interface Assignment - Exercise 1");
+        setSize(400, 300);
+        // setMinimumSize(new Dimension(300, 400));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void clearButtonClicked(ActionEvent e) {
+        textArea.setText("");
+    }
+
+    public void verifyButtonClicked(ActionEvent e) {
+        String emailMessage = textArea.getText();
+        int spamScore = 0;
+        for (String keyPhrase : spamKeyPhrases) {
+            Pattern pattern = Pattern.compile(".*" + Pattern.quote(keyPhrase) + ".*", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(emailMessage);
+            if (matcher.matches()) {
+                spamScore++;
+            }
+        }
+        JOptionPane.showMessageDialog(this, "The spam score for this message is " + spamScore);
+    }
+}
---
+++ misc/MainFrameLearn.java
@@ -0,0 +1,95 @@
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrameLearn extends JFrame {
+    final private Font mainFont = new Font("Arial", Font.BOLD, 18);
+
+    JTextField tfFirstName, tfLastName;
+    JLabel lbWelcome;
+
+    public static void main(String[] args) {
+        MainFrameLearn mainFrame = new MainFrameLearn();
+        mainFrame.initComponents();
+    }
+
+    public void initComponents() {
+        JLabel lbFirstName = new JLabel("First Name");
+        lbFirstName.setFont(mainFont);
+
+        tfFirstName = new JTextField();
+        tfFirstName.setFont(mainFont);
+
+        JLabel lbLastName = new JLabel("Last Name");
+        lbLastName.setFont(mainFont);
+
+        tfLastName = new JTextField();
+        tfLastName.setFont(mainFont);
+
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(4, 1, 5, 5));
+        formPanel.setOpaque(false);
+        formPanel.add(lbFirstName);
+        formPanel.add(tfFirstName);
+        formPanel.add(lbLastName);
+        formPanel.add(tfLastName);
+
+        lbWelcome = new JLabel("Welcome");
+        lbWelcome.setFont(mainFont);
+
+        JButton btnOK = new JButton("OK");
+        btnOK.setFont(mainFont);
+        btnOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String firstName = tfFirstName.getText();
+                String lastName = tfLastName.getText();
+                lbWelcome.setText("Welcome " + firstName + " " + lastName);
+            }
+        });
+
+        JButton btnClear = new JButton("Clear");
+        btnClear.setFont(mainFont);
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tfFirstName.setText("");
+                tfLastName.setText("");
+                lbWelcome.setText("Welcome");
+            }
+        });
+
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout(1, 2, 5, 5));
+        buttonsPanel.setOpaque(false);
+        buttonsPanel.add(btnOK);
+        buttonsPanel.add(btnClear);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBackground(new Color(128, 128, 255));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        mainPanel.add(formPanel, BorderLayout.NORTH);
+        mainPanel.add(lbWelcome, BorderLayout.CENTER);
+        mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
+
+        add(mainPanel);
+
+        setTitle("Learn");
+        setSize(500, 600);
+        setMinimumSize(new Dimension(300, 400));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
