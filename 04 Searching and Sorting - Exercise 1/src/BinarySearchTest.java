4e4620566e9ca6cfb8d031ac45360b4a3f67e262
MattTheCuber <mattvine03@gmail.com>
2024-05-25 15:57:23 -0400

    docs(week 4): update comments and add JavaDoc

--- 04 Searching and Sorting - Exercise 1/src/BinarySearchTest.java
+++ 04 Searching and Sorting - Exercise 1/src/BinarySearchTest.java
@@ -1,3 +1,7 @@
+// Generic Collections Assignment - Exercise 2
+// Matthew Vine
+// CSIS 505-B01 (Liberty University)
+// May 31, 2024
 
 // Fig. 19.3: BinarySearchTest.java
 // Use binary search to locate an item in an array.
@@ -7,34 +11,43 @@ import java.util.Scanner;
 
 public class BinarySearchTest {
     /* CODE ADDED BY MATTHEW VINE */
+    /**
+     * Recursive binary search method to locate an item in an array.
+     * 
+     * @param data The array to search.
+     * @param key  The search key.
+     * @param low  The low end of the search area.
+     * @param high The high end of the search area.
+     * @return The location of the search key in the array. -1 if not found.
+     */
     public static int recursiveSearchVine(int[] data, int key, int low, int high) {
         int middle = (low + high + 1) / 2; // The middle element
-        int location = -1; // This will be the return value; -1 if not found
+        int location = -1; // This will be the return value; -1 if not found.
 
-        // Print the remaining elements of array
+        // Print the remaining elements of array.
         System.out.print(remainingElements(data, low, high));
 
-        // Output spaces for alignment
+        // Output spaces for alignment.
         for (int i = 0; i < middle; i++) {
             System.out.print("   ");
         }
-        System.out.println(" * "); // Indicate the current middle position
+        System.out.println(" * "); // Indicate the current middle position.
 
-        // If the element is found at the middle
+        // If the element is found at the middle.
         if (key == data[middle]) {
-            location = middle; // Location is the current middle
-        } else if (key < data[middle]) { // Else if the middle element is too high
-            high = middle - 1; // Eliminate the higher half
-        } else { // Else the middle element is too low
-            low = middle + 1; // Eliminate the lower half
+            location = middle; // Location is the current middle.
+        } else if (key < data[middle]) { // Else if the middle element is too high.
+            high = middle - 1; // Eliminate the higher half.
+        } else { // Else the middle element is too low.
+            low = middle + 1; // Eliminate the lower half.
         }
 
-        // If the low end of the search area is less than or equal to the high end
+        // If the low end of the search area is less than or equal to the high end.
         if ((low <= high) && (location == -1)) {
-            // Recursively search the array
+            // Recursively search the array.
             return recursiveSearchVine(data, key, low, high);
         } else {
-            return location; // Return location of search key
+            return location; // Return the location of search key.
         }
     }
     /* END OF ADDED CODE */
