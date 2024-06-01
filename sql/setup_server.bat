30c56e64dc19b9c58b3b044d5cd96eff1ce7468f
MattTheCuber <mattvine03@gmail.com>
2024-06-01 09:24:31 -0400

    refactor(sql): move to its own folder

--- sql/setup_server.bat
+++ sql/setup_server.bat
@@ -2,5 +2,5 @@ CALL "%JAVA_HOME%/db/bin/setEmbeddedCP.bat"
 CALL "%JAVA_HOME%/db/bin/ij"
 
 REM connect 'jdbc:derby:books;create=true;user=vine;password=vine';
-REM run 'books.sql';
+REM run '.\scripts\books.sql';
 REM exit;
